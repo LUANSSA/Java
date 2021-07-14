15981b9c29467fbb2b58bbbf7b287c7c524b85a8
LUANSSA <54681373+LUANSSA@users.noreply.github.com>
2021-07-13 21:16:46 -0300

    Update and rename tipos_primitivos.java to TiposPrimitivos.java

---
+++ TiposPrimitivos.java
@@ -0,0 +1,21 @@
+public class TiposPrimitivos{
+
+  public static void main(String[] args){
+    //int
+
+    //double
+
+    //float
+
+    //boolean
+
+
+    //char
+
+    //byte
+
+    //short
+
+    //long
+  }
+}
