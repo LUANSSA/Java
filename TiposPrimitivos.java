8fbe7f3e250936ead2849cb199bc3cd933933ae0
LUANSSA <54681373+LUANSSA@users.noreply.github.com>
2021-07-13 21:25:38 -0300

    Update TiposPrimitivos.java

--- TiposPrimitivos.java
+++ TiposPrimitivos.java
@@ -5,20 +5,32 @@ public class TiposPrimitivos{
     
     //int 
     int idade = 24;
-    
     //double
     double saldoBancario = 401.04;
     //float
-
+    float  altura = 170f;
     //boolean
-
-
+    boolean trabalha = true;
     //char
-
+    char sexo = "M";
     //byte
-
+    int qtDedosNasMaos = 10;
     //short
-
+    short qtDedosNosPes = 10;
     //long
+    long numeroGrande = 10000L;
+    //String
+    String nome = "Luan";
+    
+    
+    System.out.println("Nome: "+nome);
+    System.out.println("Idade: "+idade);
+    System.out.println("Altura: "+altura);
+    System.out.println("Trabalha: "+trabalha);
+    System.out.println("Sexo: "+sexo);
+    System.out.println("Saldo Bancário: "+saldoBancario);
+    System.out.println("Quantidade de dedos nas Mãos: "+qtDedosNasMaos);
+    System.out.println("Quantidade de dedos nos Pés: "+qtDedosNosPes);
+    System.out.println("Numero grade: "+numeroGrande);
   }
 }
