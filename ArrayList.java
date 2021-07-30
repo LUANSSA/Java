919951d045287e4139fea6b5ca3ff9445abd4db8
LUANSSA <54681373+LUANSSA@users.noreply.github.com>
2021-07-30 20:27:12 -0300

    Update ArrayList.java

--- ArrayList.java
+++ ArrayList.java
@@ -2,21 +2,21 @@ import java.util.ArrayList;
 public static void main(String[]
     //Instanciando listaNotasMatematica
     ArrayList listaNotasMatematica = new ArrayList();
-		listaNotasMatematica.add(10.0f);
-		listaNotasMatematica.add(6.0f);
-		listaNotasMatematica.add(7.0f);
-		listaNotasMatematica.add(5.5f);
+    listaNotasMatematica.add(10.0f);
+    listaNotasMatematica.add(6.0f);
+    listaNotasMatematica.add(7.0f);
+    listaNotasMatematica.add(5.5f);
 		  
-		System.out.println(listaNotasMatematica.toString());
-		System.out.println(listaNotasMatematica.contains(7.0f));
+    System.out.println(listaNotasMatematica.toString());
+    System.out.println(listaNotasMatematica.contains(7.0f));
     //Instânciando listaTeste
-		ArrayList listaTeste = new ArrayList();
-		listaTeste.add(8.0f);
-		listaTeste.add(8.0f);
+    ArrayList listaTeste = new ArrayList();
+    listaTeste.add(8.0f);
+    listaTeste.add(8.0f);
     //Adicionando listaTeste á listaNotasMatematica                    
-		listaNotasMatematica.addAll(listaTeste);
-		System.out.println(listaTeste.toString());
-		System.out.println(listaNotasMatematica.toString());
+    listaNotasMatematica.addAll(listaTeste);
+    System.out.println(listaTeste.toString());
+    System.out.println(listaNotasMatematica.toString());
   
   
   
