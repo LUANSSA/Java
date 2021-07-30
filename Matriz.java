cc9ecca9e42e00ba8c396720f45ac7ecdd280bb9
LUANSSA <54681373+LUANSSA@users.noreply.github.com>
2021-07-30 20:12:53 -0300

    Create Matriz.java

---
+++ Matriz.java
@@ -0,0 +1,42 @@
+public static void main(String[] args){
+  double[][] notas = {{1,2,3,4},{5,6,7,8},{9,10,11,12}};
+  
+	System.out.println(notas[0][0]);
+	System.out.println(notas[0][1]);
+	System.out.println(notas[0][2]);
+	System.out.println(notas[0][3]);
+		
+	System.out.println(notas[1][0]);
+	System.out.println(notas[1][1]);
+	System.out.println(notas[1][2]);
+	System.out.println(notas[1][3]);
+  
+  String[][] nomes = new nomes[3][4];
+  nomes[0][0] = "Luan";
+  nomes[0][1] = "Carlos";
+  nomes[0][2] = "Jaim";
+  nomes[0][3] = "Lucan";
+  
+  nomes[1][0] = "Luan";
+  nomes[1][1] = "Carlos";
+  nomes[1][2] = "Jaim";
+  nomes[1][3] = "Lucan";
+  
+  nomes[2][0] = "Luan";
+  nomes[2][1] = "Carlos";
+  nomes[2][2] = "Jaim";
+  nomes[2][3] = "Lucan";
+		  
+	System.out.println(nomes[0][0]);
+	System.out.println(nomes[0][1]);
+	System.out.println(nomes[0][2]);
+	System.out.println(nomes[0][3]);
+		
+	System.out.println(nomes[1][0]);
+	System.out.println(nomes[1][1]);
+	System.out.println(nomes[1][2]);
+	System.out.println(nomes[1][3]);
+
+  
+  
+}
