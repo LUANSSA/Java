38840616240241080605ede593cfb2c5f68d52b8
LUANSSA <54681373+LUANSSA@users.noreply.github.com>
2021-07-30 17:23:56 -0300

    Create Object.java

---
+++ Object.java
@@ -0,0 +1,9 @@
+//RECEBE QUALQUER TIPO DE DADO
+public static void main(String[] args){
+  Object[] arrayObjects = new Object[5];
+  arrayObjects[0] = "Luan";
+  arrayObjects[1] = 24;
+  arrayObjects[2] = "Desenvolvimento de Sistemas";
+  arrayObjects[3] = "HubSchool";
+  arrayObjects[4] = 4.00;
+}
