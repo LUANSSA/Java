05da20acd0c76c4f699984d70ebc16bc052bbbe9
LUANSSA <54681373+LUANSSA@users.noreply.github.com>
2021-08-09 15:49:56 -0300

    Update List.java

--- List.java
+++ List.java
@@ -1,1 +1,9 @@
+//List é uma interface!
+//List não pode ser instânciada pois é uma interface
+//List pode ser implementada por outras classes como ArrayList e LinkedList
 
+
+public static void main(String[] args){
+  List list = new ArrayList();
+
+}
