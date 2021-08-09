afd09d96af3138b2f8eb3327c5c84e50b65a3ad4
LUANSSA <54681373+LUANSSA@users.noreply.github.com>
2021-08-09 15:53:03 -0300

    Update List.java

--- List.java
+++ List.java
@@ -1,9 +1,14 @@
 //List é uma interface!
 //List não pode ser instânciada pois é uma interface
 //List pode ser implementada por outras classes como ArrayList e LinkedList
-
+//Umas lista possuí nodos onde cada valor guardado tem seu nodo apontando para próxima posição na lista
+//Onde terá outro valor guardado
+//Quando o nodo de uma posição na lista é null isso indica que a lista chegou ao fim
+//Para acessar um valor na lista é preciso ir de nodo em nodo até a posição onde o valor está
+//parecido com uma pilha
 
 public static void main(String[] args){
+  //exemplo de lista sendo instanciada com uma classe ArrayList
   List list = new ArrayList();
 
 }
