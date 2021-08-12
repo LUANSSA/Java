5c3897a16a829958302f1693db9308d6b6446cc2
LUANSSA <54681373+LUANSSA@users.noreply.github.com>
2021-08-11 21:25:13 -0300

    Update Final.java

--- Final.java
+++ Final.java
@@ -1,1 +1,12 @@
-//
+//Modificador de acesso final
+public class Escola(){
+  //um atributo final não pode ser alterado e nem herdado por outra classe
+  private final double PI = 3.14f;
+  
+  //um método final não pode ser herdado por outra classe
+  //por isso ele não pode virar um Override
+  public final double getPI(){
+    return this.PI;
+  }
+  
+}
