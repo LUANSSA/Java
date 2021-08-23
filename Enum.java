cca3588a6fd5bde9b420e85f68069d5de7d523fa
LUANSSA <54681373+LUANSSA@users.noreply.github.com>
2021-08-23 19:52:01 -0300

    Update Enum.java

--- Enum.java
+++ Enum.java
@@ -9,6 +9,39 @@ DELIVERED;
 }
 
 package entities;
-public Order{
 
+import java.util.Date;
+import entities.enums.OrderStatus;
+public class Order{
+  private Integer id;
+  private Date moment;
+  private OrderStatus status;
+  
+  public Order(){
+  
+  }
+  public Order(Integer id, Date moment, OrderStatus status){
+    this.setId(id);
+    this.setDate(moment);
+    this.setOrderStatus(status);
+  }
+  
+  public Integer getId(){
+    return this.id;
+  }
+  public void setId(Integer id){
+    this.id = id;
+  }
+  public Date getDate(){
+    return this.date;
+  }
+  public void setDate(Date moment){
+    this.moment = moment;
+  }
+  public OrderStatus getOrderStatus(){
+    return this.OrderStatus;
+  }
+  public void setOrderStatus(OrderStatus status){
+    this.OrderStatus = status;
+  }
 }
