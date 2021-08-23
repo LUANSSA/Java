5c451bcb68a348c358e62d046393d9ffff3a14ba
LUANSSA <54681373+LUANSSA@users.noreply.github.com>
2021-08-23 19:44:56 -0300

    Update Enum.java

--- Enum.java
+++ Enum.java
@@ -1,1 +1,14 @@
+//Order lifecycle : PendingPayment - Processing - Shipped - Delivered 
 
+package entities.enums;
+public enum OrderStatus{
+PENDING_PAYMENT,
+PROCESSING,
+SHIIPPED,
+DELIVERED;
+}
+
+package entities;
+public Order{
+
+}
